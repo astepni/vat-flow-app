d2f1efd72ced5ff97f411898a4f8c4cc43afcab1
astepni <agnieszkastepniewska@o2.pl>
2025-10-23 01:40:49 +0200

    test: add unit tests for VAT verification endpoints

---
+++ vat_flow/api/tests/test_vat_verification_invalid_nip_format_validation.py
@@ -0,0 +1,27 @@
+from django.contrib.auth import get_user_model
+from django.test import TestCase
+
+User = get_user_model()
+
+
+class VatVerificationInvalidNipValidationTests(TestCase):
+    def setUp(self):
+        self.user = User.objects.create_user(username="testuser", password="testpass")
+
+    def test_vat_verification_invalid_nip_format_shows_error(self):
+        self.client.login(username="testuser", password="testpass")
+        # NIP za krótki
+        response_short = self.client.post("/api/vat-verification/", {"nip": "123"})
+        self.assertContains(response_short, "Nieprawidłowy format NIP")
+
+        # NIP za długi
+        response_long = self.client.post(
+            "/api/vat-verification/", {"nip": "12345678901234"}
+        )
+        self.assertContains(response_long, "Nieprawidłowy format NIP")
+
+        # NIP z literami
+        response_letter = self.client.post(
+            "/api/vat-verification/", {"nip": "12ab56789"}
+        )
+        self.assertContains(response_letter, "Nieprawidłowy format NIP")
