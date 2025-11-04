34c77fc051fcf2f77a40c0c2ba8a90dbe5fd9984
astepni <agnieszkastepniewska@o2.pl>
2025-11-04 17:55:36 +0100

    build: update Docker configuration

--- vat_flow/vat_flow/settings.py
+++ vat_flow/vat_flow/settings.py
@@ -118,14 +118,13 @@ WSGI_APPLICATION = "vat_flow.wsgi.application"
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
-        "NAME": "vatflow",
-        "USER": "vatadmin",
-        "PASSWORD": "test1234",
-        "HOST": "db",
-        "PORT": "5432",
+        "NAME": os.environ.get("POSTGRES_DB"),
+        "USER": os.environ.get("POSTGRES_USER"),
+        "PASSWORD": os.environ.get("POSTGRES_PASSWORD"),
+        "HOST": os.environ.get("POSTGRES_HOST"),
+        "PORT": os.environ.get("POSTGRES_PORT"),
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
